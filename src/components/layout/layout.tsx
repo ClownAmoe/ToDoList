9137c7faa4ca01a84bcdd82e0c20b5c10efce2f4
ClownAmoe <qqmimimiraiglhf@gmail.com>
2025-06-18 18:23:13 +0300

    addded sort of list

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,17 +2,23 @@
 import { useDispatch, useSelector } from "react-redux";
 import Task from "../task/task";
 import { AppDispatch, RootState } from "@/state/store";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { setTaskAsync, TaskProp } from "@/state/task/taskSlice";
 export default function Layout() {
   const tasks = useSelector((state: RootState) => state.task.tasks);
   const dispatch = useDispatch<AppDispatch>();
+
+  const filteredByDone = useMemo(() => {
+    return [...tasks].sort((a, b) => Number(a.isDone) - Number(b.isDone));
+  }, tasks);
+
   useEffect(() => {
     dispatch(setTaskAsync());
+    console.log("reremder");
   }, []);
   return (
     <div className="h-lvh w-2/3 flex-col bg-amber-100 rounded-lg flex items-center justify-start p-4 gap-4 overflow-y-auto scrollbar scrollbar-thumb-sky-300 scrollbar-thumb-rounded scrollbar-track-amber-200">
-      {tasks.map((task: TaskProp) => {
+      {filteredByDone.map((task: TaskProp) => {
         return (
           <Task
             id={task.id}
